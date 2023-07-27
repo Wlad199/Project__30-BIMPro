fd7b6b715c6f49973b4518d93d10e3e10196453f
Wlad199 <habilis199@mail.ru>
2023-07-27 13:07:33 +0300

    disp

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -11,3 +11,24 @@ $(document).ready(function () {
 		//adaptiveHeight: true, // адаптивная высота (false)
 	});
 })
+
+
+
+
+let parent = document.querySelector('.ask-questions');
+let menuItem = parent.querySelectorAll('.ask-questions__item');
+
+parent.addEventListener('click', (event) => {
+	// Отлавливаем элемент в родители на который мы нажали
+	let target = event.target.closest('.ask-questions__item');
+
+	// Проверяем тот ли это элемент который нам нужен
+	if (target.classList.contains('ask-questions__item')) {
+		for (let i = 0; i < menuItem.length; i++) {
+			// Убираем у других
+			menuItem[i].classList.remove('_active');
+		}
+		// Добавляем тому на который нажали
+		target.classList.add('_active');
+	}
+});
